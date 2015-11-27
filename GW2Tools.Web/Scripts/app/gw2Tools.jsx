89fdfde4fdcfb5d457aa2a824435dd80f5659eb5
devsull <devin311@gmail.com>
2015-11-27 12:48:49 -0800

    React jsx

---
+++ GW2Tools.Web/Scripts/app/gw2Tools.jsx
@@ -0,0 +1,25 @@
+﻿var Gw2Tools = React.createClass({
+    render: function () {
+        return (
+            <div className="gw2-tools">
+                <ApiKeyForm />
+            </div>
+    );
+  }
+});
+
+var ApiKeyForm = React.createClass({
+    render: function () {
+        return (
+            <div className="api-key-form">
+                <label for="api-key">GW2 Api Key</label>
+                <input type="text" name="api-key" id="api-key" />
+            </div>  
+        );
+    }
+});
+
+ReactDOM.render(
+    <Gw2Tools />,
+    document.getElementById('content')
+);
