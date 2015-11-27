d97b6b6e933d8cc692cbee1b5e28abf38b3dd356
devsull <devin311@gmail.com>
2015-11-27 15:44:29 -0800

    Added buttons and stuff

--- GW2Tools.Web/Scripts/app/gw2Tools.jsx
+++ GW2Tools.Web/Scripts/app/gw2Tools.jsx
@@ -12,14 +12,52 @@ var ApiKeyForm = React.createClass({
     render: function () {
         return (
             <div className="api-key-form">
-                <label for="api-key">GW2 Api Key</label>
-                <input type="text" name="api-key" id="api-key" />
+                <InputField id="api-key" type="text" placeholder="Enter GW2 API Key" button="Enter">
+                    GW2 API Key
+                </InputField>
             </div>  
         );
+    }
+});
+/*
+
+                <label for="api-key">GW2 Api Key</label>
+                <input type="text" name="api-key" id="api-key" />
+*/
+var InputField = React.createClass({
+   render: function () {
+       var buttonText = this.props.button;
+       if(buttonText != null) {
+           button = <InputFieldButton type="primary">{buttonText}</InputFieldButton>
+       }
+       return (
+           <div className="input-group">
+                <span className="input-group-addon" id={this.props.id}>
+                    {this.props.children}
+                </span>
+                <input type={this.props.type} className="form-control" placeholder={this.props.placeholder} aria-describedby={this.props.id} />
+                {button}
+           </div>
+       );
+   } 
+});
+
+var InputFieldButton = React.createClass({
+    render: function () {
+        var buttonType = this.props.type != null ? this.props.type : "primary";
+        
+        var classes = "btn btn-" + buttonType;
+        return (
+            <span className="input-group-btn">
+                <button className={classes} type>
+                    {this.props.children}
+                </button>
+            </span>
+        );       
     }
 });
 
 ReactDOM.render(
     <Gw2Tools />,
-    document.getElementById('content')
+    document.getElementById('tools')
 );
