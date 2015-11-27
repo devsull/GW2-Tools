c6c538b61e8708b7f624ee7452bdbc1c612f549b
devsull <devin311@gmail.com>
2015-11-27 08:06:04 -0800

    Cleaned up web layer, doesn't do anything yet.
    Deleted Angular2 because it wouldn't compile correctly and would rather
    spend my time doing something else....

---
+++ GW2Tools.Web/Scripts/app/gw2Tools.js
@@ -0,0 +1,2 @@
+///// <reference path="../node_modules/angular2/angular2.d.ts" />
+//# sourceMappingURL=gw2Tools.js.map
---
+++ GW2Tools.Web/Scripts/app/gw2Tools.ts
@@ -0,0 +1,15 @@
+﻿///// <reference path="../node_modules/angular2/angular2.d.ts" />
+
+//import {bootstrap, Component, FORM_DIRECTIVES} from "../node_modules/angular2/angular2";
+
+//@Component({
+//    selector: 'gw2-tools',
+//    template: `<div>Loaded!</div>`,
+//    directives: [FORM_DIRECTIVES]
+//});
+
+//class MyGw2Tools {
+//    public api_key:string;
+//}
+
+//bootstrap(MyGw2Tools);
