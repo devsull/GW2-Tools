b318b38fc95c64f221060d2ba6af194adc388213
devsull <devin311@gmail.com>
2015-12-02 06:26:21 -0800

    documentation

--- GW2Tools.Web/Controllers/DefaultController.cs
+++ GW2Tools.Web/Controllers/DefaultController.cs
@@ -1,11 +1,27 @@
-﻿using System.Web.Mvc;
-using GW2Tools.Core;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DefaultController.cs" company="Devin Sullivan">
+//   copy write
+// </copyright>
+// <summary>
+//   Defines the DefaultController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace GW2Tools.Web.Controllers
 {
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The default controller.
+    /// </summary>
     public class DefaultController : Controller
     {
-        // GET: Default
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
         public ActionResult Index()
         {
             return View();
